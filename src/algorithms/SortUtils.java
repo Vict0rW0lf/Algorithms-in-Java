f8bf6be13ada1c4940ba65754a16ab0b9e3e8982
diowolf <diorgenes_tex@live.com>
2019-04-27 14:33:27 -0300

    Create UTIL for  sorting

---
+++ src/algorithms/SortUtils.java
@@ -0,0 +1,11 @@
+package algorithms;
+
+import java.util.Arrays;
+
+final class SortUtils {
+
+	static void print(Object[] toPrint){
+        System.out.println(Arrays.toString(toPrint));
+    }
+	
+}
